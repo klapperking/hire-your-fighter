704601018af66d1e848c57b53f37cab3200ac5ea
Martin Klapper <martinklapper@tutanota.com>
2023-05-30 11:53:53 +0200

    CUD Actions and Views

--- app/controllers/fighters_controller.rb
+++ app/controllers/fighters_controller.rb
@@ -1,2 +1,51 @@
 class FightersController < ApplicationController
+  before_action :set_fighter, only: %i[edit update destroy]
+
+  # GET '/fighters/new'
+  def new
+    @fighter = Fighter.new
+  end
+
+  # POST '/fighters/'
+  def create
+    new_fighter = Fighter.new(fighter_params)
+
+    if new_fighter.save
+      # redirect to '/fighters'
+      redirect_to(fighters_path)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # GET '/fighters/:id/edit'
+  def edit; end
+
+  # PATCH/PUT '/fighters/:id/'
+  def update
+    fighter_to_edit = Fighter.new(fighter_params)
+    if fighter_to_edit.update
+      redirect_to(fighter_path(fighter_to_edit))
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE '/fighters/:id'
+  def destroy
+    @fighter.destroy
+    redirect_to(fighters_path)
+  end
+
+  private
+
+  def fighter_params
+    params.require(:fighter).permit(%i[])
+  end
+
+  protected
+
+  def set_fighter
+    @fighter = Fighter.find(params[:id])
+  end
 end

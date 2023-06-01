03034732dcb1381faf2cb72691670d22505ebd9e
Martin Klapper <martinklapper@tutanota.com>
2023-06-01 21:31:06 +0200

    Styling change - hover background nto working yet

--- app/javascript/controllers/hover_controller.js
+++ app/javascript/controllers/hover_controller.js
@@ -2,13 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="hover"
 export default class extends Controller {
-  static targets = ["image", "strength", "defense", "speed"]
+  static targets = ["card", "image", "name", "strength", "defense", "speed"]
 
   connect() {
   }
 
   updateHover(event) {
     this.imageTarget.src = event.currentTarget.dataset.image
+    this.cardTarget.style.backgroundImage = `${event.currentTarget.dataset.asseturl}` // FIX (Does not work but URL is correct.)
+    this.nameTarget.innerHTML = `<strong>${event.currentTarget.dataset.name}</strong>`
     this.strengthTarget.innerHTML = `<strong>Strength: ${event.currentTarget.dataset.strength}</strong>`
     this.defenseTarget.innerHTML = `<strong>Defense: ${event.currentTarget.dataset.defense}</strong>`
     this.speedTarget.innerHTML = `<strong>Speed: ${event.currentTarget.dataset.speed}</strong>`

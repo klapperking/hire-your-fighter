a63f440ed27eb6479a2070bc1eef2c5033083c73
Martin Klapper <martinklapper@tutanota.com>
2023-06-02 08:15:18 +0200

    New images and hover update; TODO: Fix Stats

--- app/javascript/controllers/hover_controller.js
+++ app/javascript/controllers/hover_controller.js
@@ -8,8 +8,12 @@ export default class extends Controller {
   }
 
   updateHover(event) {
+
+    // update Image and Background with appropriate card
     this.imageTarget.src = event.currentTarget.dataset.image
-    this.cardTarget.style.backgroundImage = `${event.currentTarget.dataset.asseturl}` // FIX (Does not work but URL is correct.)
+    this.cardTarget.style.backgroundImage = `url(${event.currentTarget.dataset.asseturl})`
+
+    // update fighter stats with the hovered stats
     this.nameTarget.innerHTML = `<strong>${event.currentTarget.dataset.name}</strong>`
     this.strengthTarget.innerHTML = `<strong>Strength: ${event.currentTarget.dataset.strength}</strong>`
     this.defenseTarget.innerHTML = `<strong>Defense: ${event.currentTarget.dataset.defense}</strong>`
